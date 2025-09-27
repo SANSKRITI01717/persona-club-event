e7707081223daa343e6d8177e32ec5304a6455b4
SANSKRITI01717 <peehusans17@gmail.com>
2025-09-27 19:39:09 +0530

    Add files via upload

---
+++ area_of_circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double radius, area;
+    cin >> radius;
+    area = 3.14* radius * radius;
+    cout << area << endl;
+    return 0;
+}
